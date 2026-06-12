d8f600ecf812d2f13b64e480cdc3925fce1fa2d4
Lisa Chudesnova <lizzuka@gmail.com>
2026-06-12 22:13:01 +0200

    Create components and move data to separate file

---
+++ src/filmdata.js
@@ -0,0 +1,211 @@
+export const FILMS = [
+  {
+    title: "Obsession (2025)",
+    poster: {
+      src: "poster-obsession.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/TaaDkbG3I7g",
+    screenings: [
+      {
+        date: "вс 14.06",
+        time: "14:00",
+        place: "Lucerna",
+        ticketsUrl: "https://www.kinolucerna.cz/?cinema=9&hall=34%2C35&sort=sort-by-data&projection=50772"
+      },
+      {
+        date: "вс 14.06",
+        time: "16:15",
+        place: "Světozor",
+        ticketsUrl: "https://www.kinosvetozor.cz/?cinema=3&hall=1%2C2%2C3&sort=sort-by-data&projection=50878"
+      },
+    ]
+  },
+  {
+    title: "I Swear (2025)",
+    poster: {
+      src: "poster-i_swear.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/10ynpduiRpM",
+    screenings: [
+      {
+        date: "вт 16.06",
+        time: "20:30",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=50935"
+      },
+    ]
+  },
+  {
+    title: "The Kid (1921)",
+    poster: {
+      src: "poster-kid.jpg",
+      alt: "",
+    },
+    description: "немой фильм с Чарли Чаплином",
+    infoButtonUrl: "https://youtu.be/O5WXokyub-4",
+    screenings: [
+      {
+        date: "вс 21.06",
+        time: "17:30",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=49258"
+      },
+    ]
+  },
+  {
+    title: "Interstellar (2014)",
+    poster: {
+      src: "poster-interstellar.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/LY19rHKAaAg",
+    screenings: [
+      {
+        date: "вс 21.06",
+        time: "19:15",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=49245"
+      },
+    ]
+  },
+  {
+    title: "The Emperor's New Groove (2000)",
+    poster: {
+      src: "poster-the_emperors_new_groove.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/vjlPrTzOPUI",
+    screenings: [
+      {
+        date: "ср 24.06",
+        time: "20:30",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=49247"
+      },
+    ]
+  },
+
+  {
+    title: "Stray Dog (1949)",
+    poster: {
+      src: "poster-stray_dog.jpg",
+      alt: "",
+    },
+    description: "фильм на японском, субтитры чешские и английские",
+    infoButtonUrl: "https://youtu.be/afA0M0rO72I",
+    screenings: [
+      {
+        date: "чт 25.06",
+        time: "18:00",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=49248"
+      },
+    ]
+  },
+
+  {
+    title: "Aero naslepo",
+    description: "Když zkusíte „Aero naslepo“, nebudete vědět, na jaký film jste se vypravili. Předem neplatíte nic. Po projekci dostanete možnost ocenit zážitek částkou, kterou si sami určíte.",
+    infoButtonUrl: "https://boxd.it/uqXaW",
+    infoButtonDescription: "Список фильмов предыдущих сеансов",
+    screenings: [
+      {
+        date: "ср 1.07",
+        time: "20:30",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=48280"
+      },
+    ]
+  },
+
+  {
+    title: "Big Trouble in Little China (1986)",
+    poster: {
+      src: "poster-big_trouble_in_little_china.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/AXsBBqPb5YE",
+    screenings: [
+      {
+        date: "чт 2.07",
+        time: "20:30",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=49256"
+      },
+    ]
+  },
+  {
+    title: "Hard Boiled (1992)",
+    poster: {
+      src: "poster-hard_boiled.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/N_XBwks_MB8",
+    screenings: [
+      {
+        date: "пн 20.07",
+        time: "20:30",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=49257"
+      },
+    ]
+  },
+  {
+    title: "Angel’s Egg (1985)",
+    poster: {
+      src: "poster-angels_egg.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/SqyVGzaQFpQ",
+    screenings: [
+      {
+        date: "чт 23.07",
+        time: "18:00",
+        place: "Aero",
+        ticketsUrl: "https://www.kinoaero.cz/?cinema=1&sort=sort-by-data&projection=50684"
+      },
+    ]
+  },
+  {
+    title: "Matrix (1999)",
+    poster: {
+      src: "poster-matrix.jpg",
+      alt: "",
+    },
+    infoButtonUrl: "https://youtu.be/d0XTFAMmhrE",
+    screenings: [
+      {
+        date: "вт 11.08",
+        time: "20:30",
+        place: "Bio Oko",
+        ticketsUrl: "https://biooko.net/?cinema=2&sort=sort-by-data&projection=50495"
+      },
+    ]
+  },
+];
+
+
+
+/*
+
+ {
+    title: "",
+    poster: {
+      src: "",
+      alt: "",
+    },
+    description: "",
+    infoButtonUrl: "",
+    screenings: [
+      {
+        date: "",
+        time: "",
+        place: "",
+        ticketsUrl: ""
+      },
+    ]
+  },
+
+*/
